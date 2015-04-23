82644cc4abd4743cffee71307c2d9eae13c9e4b9
robsongajunior <robson.ga.junior@gmail.com>
2015-04-23 01:16:20 -0300

    [*] update - sociais buttons/scripts/style responsive

---
+++ resources/js/socialnetwork.js
@@ -0,0 +1,51 @@
+(function SocialNetwork() {
+
+	/**
+	 *
+	 * FACEBOOK LIKE BUTTON
+	 *
+	 **/
+	(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s);
+		js.id = id;
+		js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+
+	/**
+	 *
+	 * TWITTER SHARE BUTTON
+	 *
+	 **/
+	! function(d, s, id) {
+		var js,
+			fjs = d.getElementsByTagName(s)[0],
+			p = /^http:/.test(d.location) ? 'http' : 'https';
+
+		if (!d.getElementById(id)) {
+			js = d.createElement(s);
+			js.id = id;
+			js.src = p + '://platform.twitter.com/widgets.js';
+			fjs.parentNode.insertBefore(js, fjs);
+		}
+	}(document, 'script', 'twitter-wjs');
+
+
+	/**
+	 *
+	 * GOOGLE PLUS LIKE BUTTON
+	 *
+	 **/
+	(function() {
+		var po = document.createElement('script');
+
+		po.type = 'text/javascript';
+		po.async = true;
+		po.src = 'https://apis.google.com/js/platform.js';
+
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(po, s);
+	})();
+})();
